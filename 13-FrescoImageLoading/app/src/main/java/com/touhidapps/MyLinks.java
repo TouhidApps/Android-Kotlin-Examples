fadf5bbd5d13db99c559770ae55f416a965133ef
TouhidApps <touhidapps@gmail.com>
2020-04-14 00:26:35 +0600

    Fresco image load to kotlin

--- 13-FrescoImageLoading/app/src/main/java/com/touhidapps/MyLinks.java
+++
@@ -1,8 +0,0 @@
-package com.touhidapps;
-
-public class MyLinks {
-    public static final String IMAGE_ONE = "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
-    public static final String IMAGE_TWO = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg";
-    public static final String IMAGE_THREE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw-w1e-ETmJbo64K5dBhCen7yCum6A9gU9wBdV4ZK5abHQlpVv";
-    public static final String IMAGE_FOUR = "https://quotesblog.net/wp-content/uploads/2018/09/A-Wonderful-Good-Morning-Card..gif";
-}
---
+++ 13-FrescoImageLoading/app/src/main/java/com/touhidapps/MyLinks.kt
@@ -0,0 +1,8 @@
+package com.touhidapps
+
+object MyLinks {
+    const val IMAGE_ONE = "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    const val IMAGE_TWO = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+    const val IMAGE_THREE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw-w1e-ETmJbo64K5dBhCen7yCum6A9gU9wBdV4ZK5abHQlpVv"
+    const val IMAGE_FOUR = "https://quotesblog.net/wp-content/uploads/2018/09/A-Wonderful-Good-Morning-Card..gif"
+}
